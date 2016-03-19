033cf53879fac52159905e2074b98fe1882c8404
Rowan Hill <rowan.hill@softwire.com>
2016-03-19 08:54:13 +0000

    Refactor ParamMapBuilder

--- annotations/src/main/java/jerseywiremock/annotations/ParamMapBuilder.java
+++ annotations/src/main/java/jerseywiremock/annotations/ParamMapBuilder.java
@@ -7,71 +7,101 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class ParamMapBuilder {
     public Map<String, Object> getParamMap(Object[] parameters, Class<?> resourceClass, String methodName) {
         Method method = ReflectionHelper.getMethod(resourceClass, methodName);
 
-        List<String> paramNames = new ArrayList<String>();
-        Map<String, Class<? extends ParamFormatter>> formatters = new HashMap<String, Class<? extends ParamFormatter>>();
+        LinkedList<ParameterDescriptor> parameterDescriptors = getParameterDescriptors(method);
+
+        if (parameterDescriptors.size() != parameters.length) {
+            throw new RuntimeException("Invocation of " + methodName + " had " + parameters.length + " params, but " + parameterDescriptors.size() + " are desired");
+        }
+
+        return buildParamMap(parameters, parameterDescriptors);
+    }
+
+    private LinkedList<ParameterDescriptor> getParameterDescriptors(Method method) {
+        LinkedList<ParameterDescriptor> parameterDescriptors = new LinkedList<ParameterDescriptor>();
         for (Annotation[] parameterAnnotations : method.getParameterAnnotations()) {
-            String paramName = null;
-            Class<? extends ParamFormatter> formatter = null;
+            ParameterDescriptor parameterDescriptor = getParameterDescriptor(parameterAnnotations);
 
-            for (Annotation parameterAnnotation : parameterAnnotations) {
-                if (parameterAnnotation instanceof QueryParam) {
-                    paramName = ((QueryParam) parameterAnnotation).value();
-                } else if (parameterAnnotation instanceof PathParam) {
-                    paramName = ((PathParam) parameterAnnotation).value();
-                } else if (parameterAnnotation instanceof ParamFormat) {
-                    formatter = ((ParamFormat) parameterAnnotation).value();
-                }
+            if (parameterDescriptor.paramName != null) {
+                parameterDescriptors.add(parameterDescriptor);
             }
+        }
+        return parameterDescriptors;
+    }
 
-            if (paramName != null) {
-                paramNames.add(paramName);
-                if (formatter != null) {
-                    formatters.put(paramName, formatter);
-                }
+    private ParameterDescriptor getParameterDescriptor(Annotation[] parameterAnnotations) {
+        ParameterDescriptor parameterDescriptor;
+
+        String paramName = null;
+        Class<? extends ParamFormatter> formatter = null;
+        for (Annotation parameterAnnotation : parameterAnnotations) {
+            if (parameterAnnotation instanceof QueryParam) {
+                paramName = ((QueryParam) parameterAnnotation).value();
+            } else if (parameterAnnotation instanceof PathParam) {
+                paramName = ((PathParam) parameterAnnotation).value();
+            } else if (parameterAnnotation instanceof ParamFormat) {
+                formatter = ((ParamFormat) parameterAnnotation).value();
             }
         }
 
-        if (paramNames.size() != parameters.length) {
-            throw new RuntimeException("Invocation of " + methodName + " had " + parameters.length + " params, but " + paramNames.size() + " are desired");
-        }
+        parameterDescriptor = new ParameterDescriptor(paramName, formatter);
+        return parameterDescriptor;
+    }
 
+    private Map<String, Object> buildParamMap(
+            Object[] parameters,
+            LinkedList<ParameterDescriptor> parameterDescriptors
+    ) {
         Map<String, Object> paramMap = new HashMap<String, Object>();
-        for (int i = 0; i < paramNames.size(); i++) {
-            String paramName = paramNames.get(i);
+        for (int i = 0; i < parameterDescriptors.size(); i++) {
+            String paramName = parameterDescriptors.get(i).paramName;
+            Class<? extends ParamFormatter> formatterClass = parameterDescriptors.get(i).formatterClass;
             Object rawParamValue = parameters[i];
 
-            String formattedValue;
-            if (formatters.containsKey(paramName)) {
-                Class<? extends ParamFormatter> formatterClass = formatters.get(paramName);
-                ParamFormatter formatter;
-                try {
-                    formatter = formatterClass.newInstance();
-                } catch (InstantiationException e) {
-                    throw new RuntimeException(e);
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-
-                // ParamFormatter is generic, and the generic type is erased by run-time, so we can't check whether
-                // rawParamValue is of the right type or not...
-                //noinspection unchecked
-                formattedValue = formatter.format(rawParamValue);
-            } else {
-                formattedValue = rawParamValue.toString();
-            }
+            String formattedValue = getFormattedParamValue(rawParamValue, formatterClass);
 
             paramMap.put(paramName, formattedValue);
         }
-
         return paramMap;
+    }
+
+    private String getFormattedParamValue(Object rawParamValue, Class<? extends ParamFormatter> formatterClass) {
+        String formattedValue;
+        if (formatterClass != null) {
+            ParamFormatter formatter;
+            try {
+                formatter = formatterClass.newInstance();
+            } catch (InstantiationException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+
+            // ParamFormatter is generic, and the generic type is erased by run-time, so we can't check whether
+            // rawParamValue is of the right type or not...
+            //noinspection unchecked
+            formattedValue = formatter.format(rawParamValue);
+        } else {
+            formattedValue = rawParamValue.toString();
+        }
+        return formattedValue;
+    }
+
+    private static class ParameterDescriptor {
+        private final String paramName;
+        private final Class<? extends ParamFormatter> formatterClass;
+
+        public ParameterDescriptor(
+                String paramName,
+                Class<? extends ParamFormatter> formatterClass
+        ) {
+            this.paramName = paramName;
+            this.formatterClass = formatterClass;
+        }
     }
 }
