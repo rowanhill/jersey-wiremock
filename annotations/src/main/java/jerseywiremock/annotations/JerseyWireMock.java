d5422b092d18ae14245f7b340db9e907499f9875
Rowan Hill <rowan.hill@softwire.com>
2016-03-20 15:38:51 +0000

    Remove old spike annotation JerseyWireMock (used for anno. processor)

--- annotations/src/main/java/jerseywiremock/annotations/JerseyWireMock.java
+++
@@ -1,9 +0,0 @@
-package jerseywiremock.annotations;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.CLASS)
-public @interface JerseyWireMock {
-    public Class<?> value();
-}
