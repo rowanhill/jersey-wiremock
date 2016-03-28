b0b32f922ba13378a24b3f6e30fc2bcbe3fb93f1
Rowan Hill <rowan.hill@softwire.com>
2016-03-28 12:04:01 +0100

    Add DateTimeIsoFormatter to new joda module

---
+++ joda/src/main/java/jerseywiremock/joda/formatter/DateTimeIsoFormatter.java
@@ -0,0 +1,12 @@
+package jerseywiremock.joda.formatter;
+
+import jerseywiremock.formatter.ParamFormatter;
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
+
+public class DateTimeIsoFormatter implements ParamFormatter<DateTime> {
+    @Override
+    public String format(DateTime param) {
+        return param == null ? "null" : param.toString(ISODateTimeFormat.dateTime());
+    }
+}
