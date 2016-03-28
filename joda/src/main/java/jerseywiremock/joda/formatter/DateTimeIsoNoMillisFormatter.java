12e135de4649c0d15a7dec9df01ed1a4d4c090ae
Rowan Hill <rowan.hill@softwire.com>
2016-03-28 16:20:52 +0100

    Add ISO DateTime with no mills param formatter

---
+++ joda/src/main/java/jerseywiremock/joda/formatter/DateTimeIsoNoMillisFormatter.java
@@ -0,0 +1,12 @@
+package jerseywiremock.joda.formatter;
+
+import jerseywiremock.formatter.ParamFormatter;
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
+
+public class DateTimeIsoNoMillisFormatter implements ParamFormatter<DateTime> {
+    @Override
+    public String format(DateTime param) {
+        return param == null ? "null" : param.toString(ISODateTimeFormat.dateTimeNoMillis());
+    }
+}
---
+++ joda/src/test/java/jerseywiremock/joda/formatter/DateTimeIsoNoMillisFormatterTest.java
@@ -0,0 +1,34 @@
+package jerseywiremock.joda.formatter;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class DateTimeIsoNoMillisFormatterTest {
+    @Test
+    public void dateTimeIsIsoFormatted() {
+        // given
+        DateTimeIsoNoMillisFormatter formatter = new DateTimeIsoNoMillisFormatter();
+        DateTime dateTime = DateTime.now();
+
+        // when
+        String formattedString = formatter.format(dateTime);
+
+        // then
+        assertThat(formattedString).isEqualTo(dateTime.toString(ISODateTimeFormat.dateTimeNoMillis()));
+    }
+
+    @Test
+    public void nullIsReturnedAsString() {
+        // given
+        DateTimeIsoNoMillisFormatter formatter = new DateTimeIsoNoMillisFormatter();
+
+        // when
+        String formattedString = formatter.format(null);
+
+        // then
+        assertThat(formattedString).isEqualTo("null");
+    }
+}
