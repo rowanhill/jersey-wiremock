9ff26d35e89115980158c4f6bb560dd4fcb65163
Rowan Hill <rowan.hill@softwire.com>
2016-04-09 15:13:38 +0100

    Add LocalDate ParamFormatter

---
+++ joda/src/main/java/jerseywiremock/joda/formatter/LocalDateIsoFormatter.java
@@ -0,0 +1,12 @@
+package jerseywiremock.joda.formatter;
+
+import jerseywiremock.annotations.formatter.ParamFormatter;
+import org.joda.time.LocalDate;
+import org.joda.time.format.ISODateTimeFormat;
+
+public class LocalDateIsoFormatter implements ParamFormatter<LocalDate> {
+    @Override
+    public String format(LocalDate param) {
+        return param == null ? "null" : param.toString(ISODateTimeFormat.date());
+    }
+}
